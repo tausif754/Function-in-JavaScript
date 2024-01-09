caedfa50b4d3ae80b523275b795f9124bbd60f1f
Tausif Ahmad <tausifahmad754@gmail.com>
2024-01-09 17:04:59 +0530

    Function in JavaScript

--- Question-6.js
+++ Question-6.js
@@ -4,7 +4,6 @@ version of that function. The curried function should accept arguments one at a 
 function until all arguments are provided. Then, it should execute the original function with all arguments.
 Test the curry function with a function that adds two numbers.
 */
-
 function curry(func) {
   const n = func.length;
   function curried(...args) {
@@ -16,7 +15,6 @@ function curry(func) {
   }
   return curried;
 }
-
 function add(a, b) {
   return a + b;
 }
